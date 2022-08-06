8567e8380fcbda2a35eb357e0d00153562234f70
Johnson Duong <71087545+johnsonduong@users.noreply.github.com>
2022-08-06 13:20:29 -0400

    Create card entries

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import { React, useEffect, useState } from "react";
+import { Container, Button, Card, Navbar, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [backendData, setBackendData] = useState([{}]);
@@ -11,7 +13,42 @@ function App() {
       });
   }, []);
 
-  return <div>{typeof backendData.dreams === "undefined" ? <p>Loading...</p> : backendData.dreams.map((dream) => <p>{dream}</p>)}</div>;
+  return (
+    <div>
+      <CustomNavbar />
+      <Container>
+        <Button>New Entry</Button>
+        {typeof backendData.posts === "undefined" ? <p>Loading...</p> : backendData.posts.map((dream) => <DreamCard title={dream.title} date={dream.date} body={dream.body} />)}
+      </Container>
+    </div>
+  );
+}
+
+function DreamCard(props) {
+  return (
+    <Card border="dark" className="my-3">
+      <Card.Header>{props.date}</Card.Header>
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text>{props.body}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+function CustomNavbar() {
+  return (
+    <Navbar bg="light" variant="light" className="mb-5">
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default App;
